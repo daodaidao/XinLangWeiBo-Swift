ae8b0fd50a42357a7833053d3e7202544c1026b7
JayShang <yijieshang@gmail.com>
2018-05-04 16:19:13 +0800

    设置返回按钮

--- WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Main/WBNavigationController.swift
+++ WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Main/WBNavigationController.swift
@@ -17,23 +17,45 @@ class WBNavigationController: UINavigationController {
     
     
     
-
+    
     //重写push方法 所有push动作都会调用此方法
+    //viewController 是被push的控制器，可以设置它的左侧的按钮作为返回按钮
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         
         //如果不是栈底控制器才需要隐藏，根控制器不需要处理
         if childViewControllers.count > 0 {
+          
             //隐藏底部的 TabBar
             viewController.hidesBottomBarWhenPushed = true
+          
+            //判断控制器的类型
+            if let vc = viewController as? WBBaseViewController {
+                
+                var title = "返回"
+                //判断控制器的及数,只有一个子控制器的时候，显示栈底控制的标题
+                if childViewControllers.count == 1 {
+                    //title显示首页的标题
+                    title = childViewControllers.first?.title ?? "返回"
+                    
+                }
+               
+                //取出自定义的 navItem
+                vc.navItem.leftBarButtonItem = UIBarButtonItem(title: title, target: self, action: #selector(popToParent))
+            
+            }
             
         }
         
-       
         
         super.pushViewController(viewController, animated: true)
         
         
-        
-
     }
+    //POP 返回到上一级控制器
+    @objc private func popToParent(){
+        popViewController(animated: true)
+        
+    }
+    
+    
 }
