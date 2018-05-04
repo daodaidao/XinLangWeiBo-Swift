262e563173faf5b8614e39e6d1e0c846f3e1e8f4
JayShang <yijieshang@gmail.com>
2018-05-04 15:33:56 +0800

    不停的push

--- WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Main/测试/WBDemoViewController.swift
+++ WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Main/测试/WBDemoViewController.swift
@@ -12,10 +12,31 @@ class WBDemoViewController: WBBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-     
+        
+        //设置标题
+        title = "第\(navigationController?.childViewControllers.count ?? 0)个"
+    }
     
+    
+    /// 继续push一个新的控制器
+    @objc private func showNext(){
+        let vc = WBDemoViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 
-
+    //重写父类的方法
+    override func setupUI() {
+        super.setupUI()
+        //设置右侧控制器
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "下一个", style: .plain, target: self, action: #selector(showNext))
+    }
+    
+    
+    
 }
+
+
+
+
+
+
