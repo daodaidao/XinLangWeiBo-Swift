d14bbbfe7675651eeb10332477509ffb188e193a
JayShang <yijieshang@gmail.com>
2018-05-04 15:45:13 +0800

    把重复的代码写成分类

--- WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Home/WBHomeViewController.swift
+++ WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Home/WBHomeViewController.swift
@@ -33,7 +33,15 @@ class WBHomeViewController: WBBaseViewController {
         super.setupUI()
         
         //设置导航栏按钮
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "好友", style: .plain, target: self, action: #selector(showFriends))
+        //无法高亮
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "好友", style: .plain, target: self, action: #selector(showFriends))
+        
+        //自定义导航栏按钮 加高亮颜色
+        //Swift 调用 OC返回 instancetype的方法，判断不出是否可选，加个UIButton类型就不用解包了
+        let btn : UIButton = UIButton.yw_textButton("好友", fontSize: 16, normalColor: UIColor.darkGray, highlightedColor: UIColor.orange)
+        btn.addTarget(self, action: #selector(showFriends), for: .touchUpInside)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
+        
         
         
     }
