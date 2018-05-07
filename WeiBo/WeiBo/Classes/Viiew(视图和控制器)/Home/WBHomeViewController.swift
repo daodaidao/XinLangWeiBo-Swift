b5d617afa0eedcace0e9601677f11b6abdb1c2ba
JayShang <yijieshang@gmail.com>
2018-05-07 10:04:29 +0800

    模拟延迟下拉刷新数据，dispatch_after回顾

--- WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Home/WBHomeViewController.swift
+++ WeiBo/WeiBo/Classes/Viiew(视图和控制器)/Home/WBHomeViewController.swift
@@ -18,10 +18,28 @@ class WBHomeViewController: WBBaseViewController {
     //加载数据
     override func loadData() {
         
-        for i in 0..<15 {
-            //将数据插入到数组顶部
-            statusList.insert(i.description, at: 0)
+        //模拟 延时 加载数据
+        //延时1秒执行
+        let time: TimeInterval = 1.0
+       
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + time) {
+            //code
+            print("1 秒后输出")
+            
+            for i in 0..<15 {
+                //将数据插入到数组顶部
+            self.statusList.insert(i.description, at: 0)
+            }
+            //结束刷新控件
+            self.refreshControl?.endRefreshing()
+    
+            //刷新表格
+            self.tableView?.reloadData()
+            
         }
+        
+        
+        
     }
     
     
@@ -32,7 +50,7 @@ class WBHomeViewController: WBBaseViewController {
         let vc = WBDemoViewController()
         
         // push 的动作是 nav做的
-    navigationController?.pushViewController(vc, animated: true)
+        navigationController?.pushViewController(vc, animated: true)
         
     }
     
@@ -42,13 +60,13 @@ class WBHomeViewController: WBBaseViewController {
         
         //设置导航栏按钮
         //无法高亮
-//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "好友", style: .plain, target: self, action: #selector(showFriends))
+        //        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "好友", style: .plain, target: self, action: #selector(showFriends))
         
         //自定义导航栏按钮 加高亮颜色
         //Swift 调用 OC返回 instancetype的方法，判断不出是否可选，加个UIButton类型就不用解包了
-//        let btn : UIButton = UIButton.yw_textButton("好友", fontSize: 16, normalColor: UIColor.darkGray, highlightedColor: UIColor.orange)
-//        btn.addTarget(self, action: #selector(showFriends), for: .touchUpInside)
-//        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
+        //        let btn : UIButton = UIButton.yw_textButton("好友", fontSize: 16, normalColor: UIColor.darkGray, highlightedColor: UIColor.orange)
+        //        btn.addTarget(self, action: #selector(showFriends), for: .touchUpInside)
+        //        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
         
         navItem.leftBarButtonItem = UIBarButtonItem(title: "好友", target: self, action: #selector(showFriends))
         
